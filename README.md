1b1fed370f55bbce9e6d4d13646c8febb3065eb5
carthworks <tkarthikeyan@gmail.com>
2023-01-28 17:44:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# interview_mach_tests
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-vcbjnx)
---
+++ script.js
@@ -0,0 +1,27 @@
+console.log('Hello!');
+
+let emp = [
+  { name: 'abc', age: 30 },
+  { name: 'bbb', age: 50 },
+  { name: 'ccc', age: 20 },
+  { name: 'ddd', age: 28 },
+  { name: 'eee', age: 40 },
+];
+
+console.log(emp);
+
+// function EmpNames() {}
+// EmpNames.prototype = {
+//   names: [],
+//   showNames: function () {
+//     return this.names;
+//   },
+// };
+
+// var e1 = new EmpNames();
+// e1.names.push('foo');
+// console.log(e1.showNames());
+
+// var e2 = new EmpNames();
+// e2.names.push('bar');
+// console.log(e2.showNames());
