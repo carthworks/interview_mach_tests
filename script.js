461e6e12b4bf605994dcc47d6b7bf43b650cdde3
carthworks <tkarthikeyan@gmail.com>
2023-01-28 23:59:12 +0530

    2 files modified

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-console.log('Hello!');
+//console.log('Hello!');
 
 let emp = [
   { name: 'abc', age: 30 },
   { name: 'bbb', age: 50 },
   { name: 'ccc', age: 20 },
   { name: 'ddd', age: 28 },
-  { name: 'eee', age: 40 },
+  { name: 'eee', age: 24 },
 ];
 
-console.log(emp);
+//console.log(emp);
+const ageBelow25 = emp.filter((employee) => employee.age < 25);
+//console.log(ageBelow25);
+var empDivIs = document.getElementById('empDiv');
+ageBelow25.map(({ name, age }) => {
+  const dataToShow = '<h4>' + name + ' and age is ' + age + '<h4>';
+  //console.log(dataToShow);
+  empDivIs.append = dataToShow;
+});
+
+empDivIs.innerHTML = JSON.stringify(ageBelow25);
 
 // function EmpNames() {}
 // EmpNames.prototype = {
